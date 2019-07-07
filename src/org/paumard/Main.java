40d1287e582b61472645321a500bf0cc90fb1e78
kasahuka <kasahuka@lin66005547.corp.capgemini.com>
2019-07-07 15:35:18 +0530

    call by value java

--- src/org/paumard/Main.java
+++ src/org/paumard/Main.java
@@ -1,36 +1,37 @@
 package org.paumard;
 
+import java.util.Comparator;
+
 /**
  *
  * @author José Paumard
  */
 public class Main {
+	
+	{
+		System.out.println("hello");
+	}
+	
+	static{
+		System.out.println(" static hello");
+	}
 
     public static void main(String[] args) {
         
-        Predicate<String> p1 = s -> s.length() < 20 ;
-        Predicate<String> p2 = s -> s.length() > 5 ;
-        
-        boolean b = p1.test("Hello");
-        System.out.println("Hello is shorter than 20 chars : " + b);
-        
-        Predicate<String> p3 = p1.and(p2) ;
+       int x=0;
+       Main m=new Main();
+       m.compute(x);
 
-        System.out.println("P3 for Yes: " + p3.test("Yes"));
-        System.out.println("P3 for Good morning: " + p3.test("Good morning"));
-        System.out.println("P3 for Good morning gentlemen: " + p3.test("Good morning gentlemen"));
-        
-        Predicate<String> p4 = p1.or(p2) ;
-        
-        System.out.println("P4 for Yes: " + p4.test("Yes"));
-        System.out.println("P4 for Good morning: " + p4.test("Good morning"));
-        System.out.println("P4 for Good morning gentlemen: " + p4.test("Good morning gentlemen"));
-        
-        Predicate<String> p5 = Predicate.isEqualsTo("Yes");
-        
-        System.out.println("P5 for Yes: " + p5.test("Yes"));
-        System.out.println("P5 for No: " + p5.test("No"));
-        
-        Predicate<Integer> p6 = Predicate.isEqualsTo(1);
+   	System.out.println(x);
     }
+    
+    public int compute(int x)
+    {
+    	System.out.println(x=x+5);
+    	return x;
+    }
+    
+   // Comparator<Integer> i= (i1,i2)->i1.compareTo(i2);
+
+    //Comparator<Integer> i= Integer::;
 }
